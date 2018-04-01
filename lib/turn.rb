f48d24c08e606922b78046a9876e52b11e2c08c1
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-01 00:02:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index || index < 0 || index > 9
+  if position_taken?(board, index) || index < 0 || index > 9
     return false
   else
     return true
